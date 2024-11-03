ce94ec337be3c92eec7c8ee8e7528e2f05d221c1
AADHIRAJAN S <103513245+Aadhirajan@users.noreply.github.com>
2024-11-03 11:36:27 +0000

    Added Information

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Aadhirajan S",
+  title: "Engineer|Developer|Activist",
+  email: "aadhirajan@proton.me",
+  gitHub: "aadhirajan",
+  instagram: "aadhi__rajan",
+  linkedIn: "Aadhirajan S",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "AadhiRajan7",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,16 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Quiz Website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A web application for creating and taking quizzes. Includes features for multiple choice questions, scoring, and user management.",
+    url: "https://github.com/aadhirajan/quiz_page",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Inventory Management System",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A system to manage inventory for small to medium-sized businesses. Features include tracking stock levels, orders, and suppliers.",
+    url: "https://github.com/aadhirajan/inventory_management_system",
   },
 ];
 
